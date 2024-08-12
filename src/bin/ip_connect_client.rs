b23a33e40809036ca5847a6a7acda869c49e8ac4
ErikBcd <Erik.Bach@gmx.de>
2024-08-13 00:58:28 +0200

    Removed unnecessary bind_addr argument

--- src/bin/ip_connect_client.rs
+++ src/bin/ip_connect_client.rs
@@ -10,13 +10,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let server_name = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:4433".to_string());
-    
-    let bind_addr = env::args()
-        .nth(2)
-        .unwrap_or_else(|| "127.0.0.1:8899".to_string());
 
     let newclient = ConnectIPClient;
-    newclient.run(&server_name, &bind_addr).await;
+    newclient.run(&server_name).await;
 
     Ok(())
 }
