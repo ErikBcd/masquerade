53734ce797a202d24f773e6e4718ec2b98b7d56a
ErikBcd <Erik.Bach@gmx.de>
2024-08-20 02:42:52 +0200

    (feat) Startup arguments  - Added args for connect-ip client  - Added arg output at start of server via info!()

--- src/bin/ip_connect_client.rs
+++ src/bin/ip_connect_client.rs
@@ -1,3 +1,5 @@
+use clap::{arg, command};
+use log::info;
 use masquerade_proxy::ip_connect::client::ConnectIPClient;
 
 use std::env;
@@ -7,24 +9,28 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::builder().format_timestamp_millis().init();
 
-    let server_name = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:4433".to_string());
+    let matches = command!()
+        .about("The CONNECT-IP client for Masquerade")
+        .arg(arg!(-s --server_name <VALUE>).default_value("127.0.0.1:4433").required(false))
+        .arg(arg!(-a --tun_addr <VALUE>).default_value("10.9.0.2/24").required(false))
+        .arg(arg!(-g --tun_gateway <VALUE>).default_value("10.9.0.1").required(false))
+        .arg(arg!(-n --tun_name <VALUE>).default_value("tunMC").required(false))
+        .get_matches();
 
-    let dev_addr = env::args()
-        .nth(2)
-        .unwrap_or_else(|| "10.9.0.2/24".to_string());
 
-    let tun_gateway = env::args()
-        .nth(2)
-        .unwrap_or_else(|| "10.9.0.1".to_string());
+    let server_name = matches.get_one::<String>("server_name").expect("Bind address not here?");
+    let tun_addr = matches.get_one::<String>("tun_addr").expect("Tun address not here?");
+    let tun_name = matches.get_one::<String>("tun_name").expect("Bind address not here?");
+    let tun_gateway = matches.get_one::<String>("tun_gateway").expect("Bind address not here?");
 
-    let tun_name = env::args()
-        .nth(3)
-        .unwrap_or_else(|| "tunMasq".to_string());
-
+    info!("Starting connect-ip client with config: \nServer address: {}\nTUN Address: {}\nTUN Name: {}\nTUN Gateway: {}",
+            server_name, tun_addr, tun_name, tun_gateway);
     let newclient = ConnectIPClient;
-    newclient.run(&server_name, dev_addr, tun_gateway, tun_name).await;
+    newclient.run(
+        server_name, 
+        tun_addr.to_owned(), 
+        tun_gateway.to_owned(), 
+        tun_name.to_owned()).await;
 
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,5 @@
-use clap::{arg, command, Command};
-use log::error;
+use clap::{arg, command};
+use log::info;
 use masquerade_proxy::server::Server;
 
 use std::env;
@@ -10,6 +10,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::builder().format_timestamp_millis().init();
 
     let matches = command!()
+        .about("The Masquerade server")
         .arg(arg!(-b --bind_addr <VALUE>).default_value("0.0.0.0:4433").required(false))
         .arg(arg!(-a --tun_addr <VALUE>).default_value("10.8.0.1/24").required(false))
         .arg(arg!(-n --tun_name <VALUE>).default_value("tunMS").required(false))
@@ -17,23 +18,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .arg(arg!(-d --link_dev <VALUE>).required(true))
         .get_matches();
 
-    println!(
-        "two: {:?}",
-        matches.get_one::<String>("bind_addr").expect("required")
-    );
-    println!(
-        "one: {:?}",
-        matches.get_one::<String>("tun_addr").expect("required")
-    );
-
-
-    
     let bind_addr = matches.get_one::<String>("bind_addr").expect("Bind address not here?");
     let tun_addr = matches.get_one::<String>("tun_addr").expect("Bind address not here?");
     let tun_name = matches.get_one::<String>("tun_name").expect("Bind address not here?");
     let local_ip = matches.get_one::<String>("local_ip").expect("Bind address not here?");
     let link_dev = matches.get_one::<String>("link_dev").expect("Bind address not here?");
 
+    info!("Starting server with config: \nUDP Bind address: {}\nTUN Address: {}\nTUN Name: {}\nLocal IP: {}\nLink Route Device: {}",
+            bind_addr, tun_addr, tun_name, local_ip, link_dev);
+    
     let mut server = Server::new();
     server.bind(bind_addr).await?;
 
